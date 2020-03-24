260b7df2b4e51b7f9277c7c592d06d9f6b32932d
Eric Montague <eric.g.montague@gmail.com>
2020-03-23 23:15:33 -0400

    added problem 104

---
+++ easy/problem_104_maximum_depth_of_binary_tree.py
@@ -0,0 +1,37 @@
+"""These are my solutions to problem 104: Maximum Depth of Binary Tree."""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+#Overall time complexity is O(n), where n is the number of nodes in the tree
+#Over space complexity is O(h), where h is the height of the binary tree
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        if root is None:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+#Overall time complexity is O(n), where n is the number of nodes in the tree
+#Over space complexity is O(h), where h is the height of the binary tree
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        max_depth = 0
+        if root is None:
+            return max_depth
+        stack = [(root, 1)]
+        while stack:
+            root, depth = stack.pop()
+            if depth > max_depth:
+                max_depth = depth
+            if root.right is not None:
+                stack.append((root.right, depth + 1))
+            if root.left is not None:
+                stack.append((root.left, depth + 1))
+        return max_depth
+
