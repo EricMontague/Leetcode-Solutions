062c20b54f48085071eaaad371fac617c48f8b9b
Eric Montague <eric.g.montague@gmail.com>
2020-06-09 12:08:49 -0400

    improved solution to be more space efficient by removing the need to keep two sets

--- medium/problem_207_course_schedule.py
+++ medium/problem_207_course_schedule.py
@@ -7,14 +7,21 @@
 
 #Overall space complexity: O(V)
 
+
+#Enumeration class used for graph coloring
+class State:
+    UNVISITED = 0
+    VISITING = 1
+    VISITED = 2
+
+
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        visited = set()
-        dfsTree = set()
+        states = [0] * numCourses
         adjacency_list = self.buildAdjList(numCourses, prerequisites)
         for course in range(numCourses):
-            if course not in visited:
-                if self.hasCycle(visited, dfsTree, course, adjacency_list):
+            if states[course] == State.UNVISITED:
+                if self.hasCycle(states, course, adjacency_list):
                     return False
         return True
     
@@ -24,14 +31,13 @@ class Solution:
             adjacency_list[prerequisite].append(course)
         return adjacency_list
     
-    def hasCycle(self, visited, dfsTree, course, prerequisites):
-        visited.add(course)
-        dfsTree.add(course)
+    def hasCycle(self, states, course, prerequisites):
+        states[course] = State.VISITING
         for nextCourse in prerequisites[course]:
-            if nextCourse not in visited:
-                if self.hasCycle(visited, dfsTree, nextCourse, prerequisites):
+            if states[nextCourse] == State.UNVISITED:
+                if self.hasCycle(states, nextCourse, prerequisites):
                     return True
             elif nextCourse in dfsTree:
                 return True
-        dfsTree.remove(course)
+        states[course] = State.VISITED
         return False
