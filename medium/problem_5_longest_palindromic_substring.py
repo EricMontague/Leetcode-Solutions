23ec50c7f9a712dbe807b7b3d178c9625866f0ba
Eric Montague <eric.g.montague@gmail.com>
2020-08-25 19:24:09 -0400

    Added dynammic programming solution

--- medium/problem_5_longest_palindromic_substring.py
+++ medium/problem_5_longest_palindromic_substring.py
@@ -1,6 +1,8 @@
 """This file contains my solution to Leetcode problem 5: Longest Palindromic Substring."""
 
 
+# Time complexity: O(n ^ 2), where 'n' is the number of characters in the string
+# Space complexity: O(1)
 class Solution:  
 
     def longestPalindrome(self, string: str) -> str:
@@ -29,3 +31,42 @@ class Solution:
         return right - left - 1
     
     
+
+# Dynamic Programming solution
+# Time complexity: O(n ^ 2), where 'n' is the number of characters in the string
+# Space complexity: O(n ^ 2), where 'n' is the number of characters in the string
+class Solution:  
+
+    def longestPalindrome(self, string: str) -> str:
+        start = 0
+        end = 0
+        memo = [[False] * len(string) for index in range(len(string))]
+        for i in range(len(string)):
+            length1 = self.expandAroundCenter(string, i, i, memo)
+            length2 = self.expandAroundCenter(string, i, i + 1, memo)
+            maxLength = max(length1, length2)
+            if maxLength > end - start:
+                mid = maxLength // 2
+                if maxLength % 2 == 0:
+                    start = i - mid + 1
+                    end = i + mid
+                else:
+                    start = i - mid
+                    end = i + mid
+        return string[start: end + 1]
+    
+    def expandAroundCenter(self, string, i, j, memo):
+        if i < 0 or j >= len(string):
+            return 0
+        if string[i] != string[j]:
+            memo[i][j] = False
+            return 0
+        if i == j: # one character at center
+            memo[i][j] = True
+        if j == i + 1 and string[i] == string[j]: # two characters at center
+            memo[i][j] = True
+        if memo[i + 1][j - 1] and string[i] == string[j]: 
+            memo[i][j] = True
+        return max(j - i + 1, self.expandAroundCenter(string, i - 1, j + 1, memo))
+        
+       
