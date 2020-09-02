fd3c320a231459c0e6679f0938095292fe7eadb0
Eric Montague <eric.g.montague@gmail.com>
2020-09-02 19:15:27 -0400

    Added new solution for problem 143 and corrected space complexity for the first solution

--- medium/problem_143_reorder_list.py
+++ medium/problem_143_reorder_list.py
@@ -2,7 +2,7 @@
 
 
 # time complexity: O(n), where 'n' is the number of nodes in the linked list
-# space complexity: O(1)
+# space complexity: O(n), where 'n' is the number of nodes in the linked list
 class Solution:
     def reorderList(self, head: ListNode) -> None:
         """
@@ -34,4 +34,95 @@ class Solution:
         if low == high:
             previous.next = nodes[low]
         nodes[low].next = None
+        
+
+# More space efficient solution
+
+# time complexity: O(n), where 'n' is the number of nodes in the linked list
+# space complexity: O(1)
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head:
+            middleNode = self.findMiddleNode(head)
+            tail = self.reverseNodes(middleNode)
+            self.reorderNodes(head, tail)
+    
+    def findMiddleNode(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def reverseNodes(self, node):
+        previous = node
+        current = node.next
+        while current:
+            next_ = current.next
+            current.next = previous
+            previous = current
+            current = next_
+        return previous
+    
+    def reorderNodes(self, head, tail):
+        sentinel = ListNode(-1, None)
+        lastNode = sentinel
+        while head != tail and head.next != tail:
+            next_ = head.next
+            lastNode.next = head
+            head.next = tail
+            lastNode = lastNode.next.next
+            head = next_
+            tail = tail.next
+        lastNode.next = head
+        tail.next = None
+        # Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: ListNode) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if head:
+            middleNode = self.findMiddleNode(head)
+            tail = self.reverseNodes(middleNode)
+            self.reorderNodes(head, tail)
+    
+    def findMiddleNode(self, head):
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def reverseNodes(self, node):
+        previous = node
+        current = node.next
+        while current:
+            next_ = current.next
+            current.next = previous
+            previous = current
+            current = next_
+        return previous
+    
+    def reorderNodes(self, head, tail):
+        sentinel = ListNode(-1, None)
+        lastNode = sentinel
+        while head != tail and head.next != tail:
+            next_ = head.next
+            lastNode.next = head
+            head.next = tail
+            lastNode = lastNode.next.next
+            head = next_
+            tail = tail.next
+        lastNode.next = head
+        tail.next = None
         
