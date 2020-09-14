9993ef3adcef05345f309715d9da9cc1d77e8140
Eric Montague <eric.g.montague@gmail.com>
2020-09-13 23:30:41 -0400

    Added preorder traversal solution

--- hard/problem_297_serialize_and_deserialize_binary_tree.py
+++ hard/problem_297_serialize_and_deserialize_binary_tree.py
@@ -69,3 +69,60 @@ class Codec:
             else:
                 nodes.append(None)
         return nodes
+
+
+# Recursive preorder traversal solution
+# time complexity: O(n), where 'n' is the number of nodes in the binary tree
+# space complexity: O(n), where 'n' is the number of nodes in the binary tree
+
+class Codec:
+    
+    SERIALIZED_NULL_POINTER = "#"
+        
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return ""
+        serializedTree = []
+        self.serializeByPreorderTraversal(root, serializedTree)
+        return ",".join(serializedTree)
+    
+    def serializeByPreorderTraversal(self, node, serializedTree):
+        if node is None:
+            serializedTree.append(self.SERIALIZED_NULL_POINTER)
+            return
+        serializedTree.append(str(node.val))
+        self.serializeByPreorderTraversal(node.left, serializedTree)
+        self.serializeByPreorderTraversal(node.right, serializedTree)
+            
+  
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if not data:
+            return None
+        stack = self.createStack(data.split(","))
+        return self.deserializeByPreorderTraversal(stack)
+    
+    def createStack(self, data):
+        stack = [data[index] for index in range(len(data) - 1, -1, -1)]     
+        return stack
+    
+    def deserializeByPreorderTraversal(self, stack):
+        if not stack:
+            return None
+        nodeValue = stack.pop()
+        if nodeValue == self.SERIALIZED_NULL_POINTER:
+            return None
+        newNode = TreeNode(int(nodeValue))
+        newNode.left = self.deserializeByPreorderTraversal(stack)
+        newNode.right = self.deserializeByPreorderTraversal(stack)
+        return newNode
