578c610012aecf8870735dd7b1ab3907419782a6
Eric Montague <eric.g.montague@gmail.com>
2020-09-19 13:09:09 -0400

    Updated solution

--- medium/problem_98_validate_binary_search_tree.py
+++ medium/problem_98_validate_binary_search_tree.py
@@ -1,37 +1,22 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
 
 class Solution:
+    
     def isValidBST(self, root: TreeNode) -> bool:
-        if root is None:
-            return True
-        last_seen = float("-inf")
-
-        #initialize stack to hold nodes
         stack = []
-        while stack or root:
-            #traverse left subtree
-            #go as far left as possible until you hit a null pointer
-            while root:
+        lastNodeSeen = None
+        while True:
+            if root is not None:
                 stack.append(root)
                 root = root.left
-
-            #pop node off of the stack and compare it to the 
-            #last seen value
-            root = stack.pop()
-            if last_seen >= root.val:
-                return False
-            last_seen = root.val
-
-            #you have finished traversing the left subtree,
-            #now you need to traverse the right subtree
-            root = root.right
-        return True
-            
+            elif stack:
+                node = stack.pop()
+                if lastNodeSeen and lastNodeSeen.val >= node.val:
+                    return False
+                lastNodeSeen = node
+                root = node.right
+            else:
+                return True
+  
 
 #Time complexity: O(n) since you visit all of the nodes in the BST at most once
 
