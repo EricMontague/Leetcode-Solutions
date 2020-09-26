28d226c6345ab6f2117598b218fecb45b059aea0
Eric Montague <eric.g.montague@gmail.com>
2020-09-26 15:35:40 -0400

    Added solution to one of the follow-up questions

--- easy/problem_350_intersection_of_two_arrays_2.py
+++ easy/problem_350_intersection_of_two_arrays_2.py
@@ -22,3 +22,28 @@ class Solution:
                 intersection.append(num)
                 counter[num] -= 1
         return intersection
+
+
+# Solution to follow-up with sorted array
+# The below solution assumes that both input arrays are sorted
+# time complexity: O(max(n, m))
+# space complexity: O(1)
+
+
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        intersection = []
+        left = 0
+        right = 0
+        nums1Length = len(nums1)
+        nums2Length = len(nums2)
+        while left < nums1Length and right < nums2Length:
+            if nums1[left] == nums2[right]:
+                intersection.append(nums1[left])
+                left += 1
+                right += 1
+            elif nums1[left] < nums2[right]:
+                left += 1
+            else:
+                right += 1
+        return intersection
