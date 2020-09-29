450d9d84bead4ccd75034cf4238c02dd95937b9b
Eric Montague <eric.g.montague@gmail.com>
2020-09-29 15:31:32 -0400

    Updated solutions

--- easy/problem_680_valid_palindrome_2.py
+++ easy/problem_680_valid_palindrome_2.py
@@ -14,33 +14,26 @@ class Solution:
             return self.isPalindrome(string, start + 1, end - 1, usedDeletion)
         if usedDeletion:
             return False
-        if self.isPalindrome(string, start + 1, end, True):
-            return True    
-        return self.isPalindrome(string, start, end - 1, True)
+        return self.isPalindrome(string, start + 1, end, True) or self.isPalindrome(string, start, end - 1, True)
 
 
 # time complexity: O(n), where 'n' is the number of characters in the string
 # space complexity: O(1)
 class Solution:
     def validPalindrome(self, string: str) -> bool:
-        return self.isPalindrome(string, "start") or self.isPalindrome(string, "end")
-     
-    
-    def isPalindrome(self, string, pointer):
         start = 0
         end = len(string) - 1
-        usedDeletion = False
         while start < end:
             if string[start] != string[end]:
-                if usedDeletion:
-                    return False
-                if pointer == "start" and string[start + 1] == string[end]:
-                    start += 1
-                elif pointer == "end" and string[start] == string[end - 1]:
-                    end -= 1
-                else:
-                    return False
-                usedDeletion = True
+                return self.isPalindrome(string, start + 1, end) or self.isPalindrome(string, start, end - 1)
+            start += 1
+            end -= 1
+        return True
+    
+    def isPalindrome(self, string, start, end):
+        while start < end:
+            if string[start] != string[end]:
+                return False
             start += 1
             end -= 1
         return True
