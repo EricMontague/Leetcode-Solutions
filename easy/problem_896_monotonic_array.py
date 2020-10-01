ffcf220a7a1b9a6b3f5702bff8f186ddab63c1c1
Eric Montague <eric.g.montague@gmail.com>
2020-10-01 00:22:28 -0400

    Added another solution

--- easy/problem_896_monotonic_array.py
+++ easy/problem_896_monotonic_array.py
@@ -14,5 +14,31 @@ class Solution:
             if isIncreasing and isDecreasing:
                 return False
         return True
-                
-        
+
+
+# Alternative solution
+# time complexity: O(n), where 'n' is the number of elements in the array
+# space complexity: O(1)
+class Solution:
+    def isMonotonic(self, nums: List[int]) -> bool:
+        direction = 0
+        for index in range(len(nums) - 1):
+            newDirection = self.calcDirection(nums[index + 1], nums[index])
+            if direction == 0:
+                direction = newDirection
+            else:
+                if direction > 0 and newDirection < 0:
+                    return False
+                if direction < 0 and newDirection > 0:
+                    return False
+        return True
+
+    def calcDirection(self, num1, num2):
+        if num1 - num2 == 0:
+            direction = 0
+        elif num1 - num2 > 0:
+            direction = 1
+        else:
+            direction = -1
+        return direction
+
