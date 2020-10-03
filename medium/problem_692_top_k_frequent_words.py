62112dc07ba630e8d14c55b7b608bcb22ca28c1a
Eric Montague <eric.g.montague@gmail.com>
2020-10-03 13:05:37 -0400

    Added Quickselect solution

--- medium/problem_692_top_k_frequent_words.py
+++ medium/problem_692_top_k_frequent_words.py
@@ -1,60 +1,133 @@
 """This is my solution to Leetcode problem 692: Top K Frequent Words."""
 
 
-from heapq import heappop, heapify, heappush
-
-
-class MaxHeapItem:
-    
-    def __init__(self, key, data):
-        self.key = key
-        self.data = data
-        
-    def __gt__(self, other):
-        if self.key == other.key:
-            return self.data > other.data
-        return self.key > other.key
-    
+# Quickselect solution
+# time complexity: O(n + klogk)
+# space complexity: O(n)
+import random
+from collections import Counter
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        if k < 1 or not words:
-            return []
-        frequencies = self.calculate_word_frequencies(words)
-        max_heap = self.build_max_heap(frequencies, k)
-        return self.get_top_k_frequent(max_heap, k)
+        word_counter = Counter(words)
+        unique_words = list(word_counter.keys())
+        kth_most_frequent_word_index = self.find_kth_most_frequent_word_index(
+            unique_words, word_counter, 0, len(unique_words) - 1, k
+        )
+        k_most_frequent_words = unique_words[kth_most_frequent_word_index:]
+        k_most_frequent_words.sort(key=lambda w: (-word_counter[w], w))
+        return k_most_frequent_words
     
-    def calculate_word_frequencies(self, words):
-        frequencies = {}
-        for word in words:
-            frequencies.setdefault(word, 0)
-            frequencies[word] += 1
-        return frequencies
+    def find_kth_most_frequent_word_index(self, words, word_counter, start, end, k):
+        if start == end:
+            return start
+        pivot_index = self.partition(words, word_counter, start, end)
+        kth_most_frequent_index = len(words) - k
+        if pivot_index == kth_most_frequent_index:
+            return pivot_index
+        elif pivot_index < kth_most_frequent_index:
+            return self.find_kth_most_frequent_word_index(
+                words, word_counter, pivot_index + 1, end, k
+            )
+        else:
+            return self.find_kth_most_frequent_word_index(
+                words, word_counter, start, pivot_index - 1, k
+            )
     
-    def build_max_heap(self, frequencies, k):
-        max_heap = [MaxHeapItem(-frequency, word) for word, frequency in frequencies.items()]
-        heapify(max_heap)
-        return max_heap
+    def partition(self, words, word_counter, start, end):
+        self.randomize_pivot_index(words, start, end)
+        pivot_value_count = word_counter.get(words[end])
+        pivot_index = start
+        for index in range(start, end):
+            word_count = word_counter.get(words[index])
+            if word_count == pivot_value_count:
+                if words[index] > words[end]:
+                    words[index], words[pivot_index] = (
+                        words[pivot_index], words[index]    
+                    )
+                    pivot_index += 1
+            elif word_count < pivot_value_count:
+                words[index], words[pivot_index] = words[pivot_index], words[index]
+                pivot_index += 1
+        words[pivot_index], words[end] = words[end], words[pivot_index]
+        return pivot_index
     
-    def get_top_k_frequent(self, max_heap, k):
-        elements = [heappop(max_heap).data for index in range(k)]
-        return elements
+    def randomize_pivot_index(self, words, start, end):
+        random_pivot_index = random.randint(start, end)
+        words[random_pivot_index], words[end] = words[end], words[random_pivot_index]
         
 
-#Overall time complexity: O(n + klogn), where "n" is the number of words and "k"
-#is the top "k" most frequent words
+# time complexity: O(nlogk), where 'n' is the number of words and 'k' is the
+# parameter we're given
+# space complexity: O(n)
+import heapq
+from collections import Counter
 
-#Explanation: It takes O(n) time to store all "n" words into the frequencies dictionary. In the worst
-#case, every word in the list will be unique so, iterating over all "n" elements in the dictionary and 
-# appending to a list takes O(n) time. Next, heapify takes O(n) time. Finally, the last method takes O(klogn) 
-# time because you have to loop "k" times and getting the maximum element out of a max heap takes O(log n) time. 
-# This makes the time complexity O(3n + klogn) which is just O(n + klogn).
+class PriorityQueueItem:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        
+    def __lt__(self, other_item):
+        if self.key == other_item.key:
+            return self.value > other_item.value
+        return self.key < other_item.key
+    
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        word_counts = Counter(words)
+        priority_queue = self.build_priority_queue(word_counts, k)
+        return self.get_top_k_frequent_words(priority_queue)
+    
+    def build_priority_queue(self, word_counts, k):
+        priority_queue = []
+        for word, count in word_counts.items():
+            queue_item = PriorityQueueItem(count, word)
+            heapq.heappush(priority_queue, queue_item)
+            if len(priority_queue) > k:
+                heapq.heappop(priority_queue)
+        return priority_queue
+    
+    def get_top_k_frequent_words(self, priority_queue):
+        top_k_frequent_words = []
+        while priority_queue:
+            queue_item = heapq.heappop(priority_queue)
+            top_k_frequent_words.append(queue_item.value)
+        return top_k_frequent_words[::-1]
 
-#Overall space complexity: O(n)
-#Explanation: In the worst case, every word in the list is unique, so the frequencues
-#dictionary would contain all "n" words. Also, the second method stores
-#all "n" elements into the max heap, making for O(n + n), which is just O(n).
 
+import heapq
+from collections import Counter
 
-#Note: Another way to do this is to use a min heap that always maintains a size of "k",
-#but that solution runs in O(nlog k) time, which is slightly worse.
+class PriorityQueueItem:
+    
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        
+    def __gt__(self, other_item):
+        if self.key == other_item.key:
+            return self.value > other_item.value
+        return self.key > other_item.key
+    
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        word_counts = Counter(words)
+        priority_queue = self.build_priority_queue(word_counts, k)
+        return self.get_top_k_frequent_words(priority_queue, k)
+    
+    def build_priority_queue(self, word_counts, k):
+        priority_queue = [
+            PriorityQueueItem(-count, word) 
+            for word, count in word_counts.items()
+        ]
+        heapq.heapify(priority_queue)
+        return priority_queue
+    
+    def get_top_k_frequent_words(self, priority_queue, k):
+        top_k_frequent_words = []
+        for num in range(k):
+            queue_item = heapq.heappop(priority_queue)
+            top_k_frequent_words.append(queue_item.value)
+        return top_k_frequent_words
