d34b3df9278e750c9a7376a4fc424688e2c8e1dd
Eric Montague <eric.g.montague@gmail.com>
2020-10-20 15:32:13 -0400

    Added a more space optimized bottom up solution

--- medium/problem_62_unique_paths.py
+++ medium/problem_62_unique_paths.py
@@ -77,3 +77,29 @@ class Solution:
                     memo[row][col] = unique_paths_down + unique_paths_right
         return memo[start_row][start_col]
     
+
+# Space optimized Bottom up Solution
+# time complexity: O(m * n), where 'm' is the number of rows and 'n' is the
+# number of columns
+
+# space complexity: O(n)
+
+# Intuition - In the bottom up solution, in order to calculate the value of memo[row][col],
+# all you need is the prior row's values. This means that you only need a list that is the
+# same size as the number of columns. Initially you fill this out with ones to represent
+# the last row in the table, which will always be filled with 1's.
+# Them you have num_rows - 2 to fill out and so that it the number of times you loop for the outer
+# loop. For the inner loop, you always know that all values in the last column of the matrix will be
+# 1, so you can skip evaluating it. Now the number of unique paths from what would be the cell below in the matrix
+# is equal to the value at the current index in the list and the number of paths from the right is equal to one
+# index ahead in the list.
+
+class Solution:
+    def uniquePaths(self, num_rows: int, num_cols: int) -> int:
+        if num_rows == 0 or num_cols == 0:
+            return 0
+        memo = [1] * num_cols
+        for row in range(num_rows - 1):
+            for col in range(num_cols - 2, -1, -1):
+                memo[col] += memo[col + 1]
+        return memo[0]
