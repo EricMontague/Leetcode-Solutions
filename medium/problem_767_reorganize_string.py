4bd4a305f02ab0ffe591f9ffea226e66307170d0
Eric Montague <eric.g.montague@gmail.com>
2020-10-23 16:56:59 -0400

    renamed files

--- medium/problem_767_reorganize_string.py
+++ medium/problem_767_reorganize_string.py
@@ -1,20 +1,53 @@
-"""This file contains my solutions to Leetcode problem 767: Reorganize String."""
+"""This file contains my solutions from Leetcode problem 767: Reorganize string."""
 
 
+
+# time complexity: O(NlogA), where 'N' is the size of the string
+# and 'A' is the size of the alphabet. Here our alphabet is stated to
+# be all lowercase letters, so the time complexity is really O(N)
+
+# space complexity: O(N)
+import heapq
 
 class Solution:
     def reorganizeString(self, string: str) -> str:
-        return self.reorganize(string, "")
+        if not string:
+            return ""
+        alphabet_size = 26
+        character_counts = self.build_character_counts(string, alphabet_size)
+        max_heap = self.build_max_heap(character_counts)
+        max_character_count = max_heap[0][0] * -1
+        if max_character_count > (len(string) + 1) // 2:
+            return ""
+        return self.build_reorganized_string(max_heap)
     
-    def reorganize(self, remaining_chars, prefix):
-        if remaining_chars == "":
-            return prefix
-        for index, char in enumerate(remaining_chars):
-            if not prefix or char != prefix[-1]:
-                reorganized_string = self.reorganize(
-                    remaining_chars[0: index] + remaining_chars[index + 1:],
-                    prefix + remaining_chars[index]
-                )
-                if reorganized_string != "":
-                    return reorganized_string
-        return ""
+    def build_character_counts(self, string, alphabet_size):
+        counts = [0] * alphabet_size
+        for character in string:
+            index = ord(character) - ord("a")
+            counts[index] += 1
+        return counts
+    
+    def build_max_heap(self, character_counts):
+        max_heap = []
+        for index, count in enumerate(character_counts):
+            if count > 0:
+                heap_item = (-count, chr(ord("a") + index))
+                heapq.heappush(max_heap, heap_item)
+        return max_heap
+    
+    def build_reorganized_string(self, max_heap):
+        reorganized_characters = []
+        while len(max_heap) > 1:
+            count1, char1 = heapq.heappop(max_heap)
+            count2, char2 = heapq.heappop(max_heap)
+            reorganized_characters.append(char1)
+            reorganized_characters.append(char2)
+            if count1 + 1 < 0:
+                heapq.heappush(max_heap, (count1 + 1, char1))
+            if count2 + 1 < 0:
+                heapq.heappush(max_heap, (count2 + 1, char2))
+        if max_heap:
+            count, char = heapq.heappop(max_heap)
+            reorganized_characters.append(char)
+        return "".join(reorganized_characters)
