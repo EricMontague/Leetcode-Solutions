7846d1d210e9dac149f71d2ca10cb6218c4fabaa
Eric Montague <eric.g.montague@gmail.com>
2020-11-04 11:00:44 -0500

    Added more solutions

--- medium/problem_518_coin_change_2.py
+++ medium/problem_518_coin_change_2.py
@@ -12,7 +12,49 @@ f(c, A, i) = f(c, A - c[i], i) + f(c, A, i - 1)
 
 """
 
+# Bottom Up Solution Space Optimized
+# time complexity: O(n * m), where 'n' is the number of coins and 'm' is the amount
+# space complexity: O(m)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        total_coins = len(coins)
+        combinations = [0] * (amount + 1)
+        combinations[0] = 1
+        for num_coins in range(total_coins):
+            for remaining_amount in range(amount + 1):
+                coin_value = coins[num_coins - 1]
+                combo1 = combinations[remaining_amount]
+                combo2 = 0
+                if coin_value <= remaining_amount:
+                    combo2 = combinations[remaining_amount - coin_value]
+                combinations[remaining_amount] = combo1 + combo2
+        return combinations[amount]
 
+# Bottom Up Solution
+# Recursion with Memoization
+# time complexity: O(n * m), where 'n' is the number of coins and 'm' is the amount
+# space complexity: O(n * m)
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        total_coins = len(coins)
+        combinations = [
+            [None] * (amount + 1)
+            for coin in range(total_coins + 1)
+        ]
+        for num_coins in range(total_coins + 1):
+            for remaining_amount in range(amount + 1):
+                if remaining_amount == 0:
+                    combinations[num_coins][remaining_amount] = 1
+                elif num_coins == 0:
+                    combinations[num_coins][remaining_amount] = 0
+                else:
+                    coin_value = coins[num_coins - 1]
+                    combo1 = combinations[num_coins - 1][remaining_amount]
+                    combo2 = 0
+                    if coin_value <= remaining_amount:
+                        combo2 = combinations[num_coins][remaining_amount - coin_value]
+                    combinations[num_coins][remaining_amount] = combo1 + combo2
+        return combinations[total_coins][amount]
 
 # Recursion with Memoization
 # time complexity: O(n * m), where 'n' is the number of coins and 'm' is the amount
@@ -31,6 +73,7 @@ class Solution:
         )
         return combos
     
+    # O(n + m)
     def compute_combinations(self, amount, coins, index, combos):
         if combos[index + 1][amount] is not None:
             return combos[index + 1][amount]
